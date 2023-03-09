b818aa76c0b9f3c69c6ce527cebbc2d0fed436a2
LianaSher <sheremetliana@gmail.com>
2023-03-09 13:51:18 +0200

    correction to Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { Overlay, ModalContainer } from '../Modal/Modal.styled';
 
 export const Modal = ({ closeModal, children }) => {
-  const handleClick = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
+  const handleClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) {
       closeModal();
     }
   };
 
   useEffect(() => {
-    document.addEventListener('keydown', handleClick);
-    return () => {
-      document.removeEventListener('keydown', handleClick);
+    const pressKey = ({ code }) => {
+      if (code === 'Escape') {
+        closeModal();
+      }
     };
-  }, []);
+
+    document.addEventListener('keydown', pressKey);
+    return () => {
+      document.removeEventListener('keydown', pressKey);
+    };
+  }, [closeModal]);
+
+  // const handleClick = ({ target, currentTarget, code }) => {
+  //   if (target === currentTarget || code === 'Escape') {
+  //     closeModal();
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   document.addEventListener('keydown', handleClick);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleClick);
+  //   };
+  // }, []);
 
   return (
     <Overlay onClick={handleClick}>
